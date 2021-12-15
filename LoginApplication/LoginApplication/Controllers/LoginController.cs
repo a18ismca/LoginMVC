b7e2eb997c0205e21d534e56f336fa6a2c0f7afe
a18ismca <a18ismca@student.his.se>
2021-12-15 21:41:28 +0100

    MVC.NET setup. For testing purposes.

---
+++ LoginApplication/LoginApplication/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoginApplication.Models;
+
+namespace LoginApplication.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login AND its view
+        public ActionResult LoginView()
+        {
+            var account = new Login() { Name = "Govno", Id = 100 };
+            return View(account);
+        }
+    }
+}
